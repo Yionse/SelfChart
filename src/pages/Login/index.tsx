4f404e010de8bd6193b69cef6f86a0018d5b4a64
Zhangtiancheng <2458015575@qq.com>
2023-09-19 10:03:36 +0800

    解决图标显示bug

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,8 +2,20 @@ import React, {useState, useRef} from 'react';
 import {Formik} from 'formik';
 import {Icon, Input, Button} from 'native-base';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {StyleSheet} from 'react-native';
 
 export default function Login() {
+  const style = StyleSheet.create({
+    inputStyle: {
+      opacity: 0.8,
+      borderWidth: 1,
+      fontSize: 16,
+      borderColor: 'transparent',
+      paddingLeft: 10,
+    },
+  });
+
   const onLogin = () => {
     console.log('点击了登录按钮');
   };
@@ -15,41 +27,34 @@ export default function Login() {
           <>
             <Input
               value={values.user}
-              bg="rgba(255,255,255, .4)"
-              _focus={{
-                backgroundColor: 'rgba(255,255,255, .8)',
-                borderColor: 'skyblue',
-                opacity: 1,
-              }}
-              opacity={0.8}
-              borderWidth={1}
-              borderRadius={20}
               placeholder="请输入账号"
-              fontSize="sm"
-              paddingLeft={6}
               onChangeText={handleChange('user')}
               onBlur={handleBlur}
-              InputLeftElement={<Icon as={AntDesign} name="user" />}
+              InputLeftElement={
+                <Icon as={AntDesign} name="user" size={6} marginLeft={2} />
+              }
+              backgroundColor="rgba(255,255,255,0.2)"
+              _focus={{opacity: 1}}
+              style={style.inputStyle}
             />
             <Input
               value={values.pass}
               type="password"
-              bg="rgba(255,255,255, .4)"
-              _focus={{
-                backgroundColor: 'rgba(255,255,255, .8)',
-                borderColor: 'skyblue',
-                opacity: 1,
-              }}
-              opacity={0.8}
-              borderWidth={1}
-              borderRadius={20}
               placeholder="请输入密码"
-              fontSize="sm"
-              paddingLeft={6}
               onChangeText={handleChange('pass')}
               onBlur={handleBlur}
-              InputLeftElement={<Icon as={AntDesign} name="user" />}
+              InputLeftElement={
+                <Icon
+                  as={MaterialIcons}
+                  name="wifi-tethering"
+                  size={6}
+                  marginLeft={2}
+                />
+              }
+              _focus={{opacity: 1}}
               mt={5}
+              backgroundColor="rgba(255,255,255,0.2)"
+              style={style.inputStyle}
             />
             <Button mt={10} borderRadius={10}>
               登录
