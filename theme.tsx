0f9d47cac4410f44b34a325b52e6a537a20f7669
Zhangtiancheng <2458015575@qq.com>
2023-09-21 17:31:04 +0800

    :rocket: 主题切换

---
+++ theme.tsx
@@ -0,0 +1,12 @@
+import {extendTheme} from 'native-base';
+
+const theme = extendTheme({
+  initTheme: {
+    borderColor: '#87CEEB',
+    primaryColor: 'skyblue',
+    tabBarActiveColor: 'rgba(135,206,235,.3)',
+    headerLeftBtn: 'black',
+  },
+});
+
+export default theme;
