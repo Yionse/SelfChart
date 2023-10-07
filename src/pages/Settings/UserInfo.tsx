2bee43913d6e281f258d346287f5c61761b8a63f
Zhangtiancheng <2458015575@qq.com>
2023-10-07 14:56:26 +0800

    :rocket:  优化

--- src/pages/Settings/UserInfo.tsx
+++ src/pages/Settings/UserInfo.tsx
@@ -1,14 +1,19 @@
 import React, {useContext} from 'react';
-import {Text, useToast} from 'native-base';
+import {Text, Button} from 'native-base';
 import {TokenContext} from '../../contexts';
 
 export default function UserInfo() {
   const {setToken, saveData} = useContext(TokenContext);
-  saveData('');
-  setToken('');
   return (
     <>
       <Text>个人信息</Text>
+      <Button
+        onPress={() => {
+          saveData('');
+          setToken('');
+        }}>
+        退出登录
+      </Button>
     </>
   );
 }
